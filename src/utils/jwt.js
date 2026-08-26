e0b0c6b512e44ce6cc66d62e9ab17fcb580a69a2
Omeshni <ovironao@gmail.com>
2026-08-26 16:28:00 +0200

    feat add JWT utility

---
+++ src/utils/jwt.js
@@ -0,0 +1,18 @@
+const jwt = require('jsonwebtoken');
+
+function generateToken(user) {
+  return jwt.sign(
+    {
+      userId: user.id,
+      role: user.role
+    },
+    process.env.JWT_SECRET,
+    {
+      expiresIn: process.env.JWT_EXPIRES_IN || '1h'
+    }
+  );
+}
+
+module.exports = {
+  generateToken
+};
