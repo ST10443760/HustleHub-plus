539bd6f4cc8ae4e524bd80ced9ca8464e5b42988
ST10391904 <st10391904@vcconnect.edu.za>
2026-08-26 16:17:17 -0800

    Added logic to check if any required fields are missing

--- Controllers/authController.js
+++ Controllers/authController.js
@@ -15,5 +15,22 @@ function getUsers(){
 }
 
 function saveUsers(users){
-fs.writeFileSync(userFile, JSON.stringify(users, null, 2));
+fs.writeFileSync(userFile, JSON.stringify(users, null, 2)
+);
+}
+
+const register = async (req, res) => {
+    try {
+        const { username, email, password } = req.body;
+
+        // Check missing fields
+        if (!username || !email || !password) {
+            return res.status(400).json({
+                success: false,
+                message: 'Username, email and password are required.'
+            });
+
+            
+        }
+    }
 }
